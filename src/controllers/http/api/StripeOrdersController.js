3bc893d602f707e8fa2df4054d36956a7648d7f1
Higor Castilho <castilhohf@gmail.com>
2020-11-28 10:23:42 -0300

    feat: receive a payload with value and quantity of the product on stripe controller

--- src/controllers/http/api/StripeOrdersController.js
+++ src/controllers/http/api/StripeOrdersController.js
@@ -2,11 +2,12 @@ const StripePaymentService = require('../../../services/payment/Stripe')
 
 class StripeOrdersController {
   async pay (req, res) {
+    const { value, quantity } = req.body
+    const formattedValue = value * 100
     const stripePaymentService = new StripePaymentService()
-    const sessionId = await stripePaymentService.createOrder(2000, 2, 'brl')
+    const sessionId = await stripePaymentService.createOrder(formattedValue, quantity, 'brl')
 
     res.json({ id: sessionId })
   }
 }
-
 module.exports = StripeOrdersController
