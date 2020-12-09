6ae6d5e63895caa5e9b263a54480bc525da2cb07
Higor Castilho <castilhohf@gmail.com>
2020-12-08 23:46:37 -0300

    test: enable cors

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,15 @@ describe('App Setup', () => {
     const response = await request(app).get('/test_x_powered_by')
     expect(response.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send('')
+    })
+
+    const res = await request(app).get('/test_cors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
