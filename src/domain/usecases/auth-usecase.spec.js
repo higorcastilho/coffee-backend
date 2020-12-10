f7984e7a7dba342b86d50bf31f64c2ffc9786d90
Higor Castilho <castilhohf@gmail.com>
2020-12-10 13:06:23 -0300

    test: add login route integration test to ensure response 200 when valid credentials are provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -157,7 +157,7 @@ describe('Auth Usecase', () => {
   test('Should call TokenGenerator with correct userId', async () => {
     const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut()
     await sut.auth('valid_email@mail.com', 'valid_password')
-    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
+    expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user._id)
   })
 
   test('Should return an accessToken if correct credentials are provided', async () => {
@@ -170,7 +170,7 @@ describe('Auth Usecase', () => {
   test('Should call UpdateAccessTokenRepository with correct values', async () => {
     const { sut, loadUserByEmailRepositorySpy, updateAccessTokenRepositorySpy, tokenGeneratorSpy } = makeSut()
     await sut.auth('valid_email@mail.com', 'valid_password')
-    expect(updateAccessTokenRepositorySpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
+    expect(updateAccessTokenRepositorySpy.userId).toBe(loadUserByEmailRepositorySpy.user._id)
     expect(updateAccessTokenRepositorySpy.accessToken).toBe(tokenGeneratorSpy.accessToken)
   })
 
--- src/main/config/routes.js
+++ src/main/config/routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const fg = require('fast-glob')
 module.exports = app => {
   app.use('/api/v1', router)
-  fg.sync('**/src/main/routes/**.js').forEach(file => {
+  fg.sync('**/src/main/routes/**routes.js').forEach(file => {
     require(`../../../${file}`)(router)
   })
 }
