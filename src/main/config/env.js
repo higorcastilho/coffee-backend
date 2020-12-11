73cd6df8b20b8351478a50db31431247a5179fc0
Higor Castilho <castilhohf@gmail.com>
2020-12-10 23:35:51 -0300

    fix: add env file to gitignore

--- src/main/config/env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/coffee-api',
-  tokenSecret: process.env.TOKEN_SECRET || 'secret',
-  port: process.env.PORT || 5858
-}
