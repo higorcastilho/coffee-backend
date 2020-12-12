82599a718f706b90e2cee080c04107220e453ccd
Higor Castilho <castilhohf@gmail.com>
2020-12-12 01:02:16 -0300

    test: ensure StripeUseCase calls StripeService with correct values

--- src/domain/usecases/stripe-usecase.spec.js
+++ src/domain/usecases/stripe-usecase.spec.js
@@ -1,6 +1,10 @@
 const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class StripeUseCase {
+  constructor ({ stripeService } = {}) {
+    this.stripeService = stripeService
+  }
+
   async pay (value, quantity, currency, orderId) {
     if (!value) {
       throw new MissingParamError('value')
@@ -17,35 +21,68 @@ class StripeUseCase {
     if (!orderId) {
       throw new MissingParamError('orderId')
     }
+
+    await this.stripeService.createOrder(value, quantity, currency, orderId)
   }
 }
 
+class StripeServiceSpy {
+  async createOrder (value, quantity, currency, orderId) {
+    this.value = value
+    this.quantity = quantity
+    this.currency = currency
+    this.orderId = orderId
+  }
+}
+
+const makeStripeService = () => {
+  const stripeServiceSpy = new StripeServiceSpy()
+  return stripeServiceSpy
+}
+
 const makeSut = () => {
-  return new StripeUseCase()
+  const stripeServiceSpy = makeStripeService()
+  const sut = new StripeUseCase({
+    stripeService: stripeServiceSpy
+  })
+
+  return {
+    sut,
+    stripeServiceSpy
+  }
 }
 
 describe('Stripe Usecase', () => {
   test('Should throw if no value is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const promise = sut.pay()
     expect(promise).rejects.toThrow(new MissingParamError('value'))
   })
 
   test('Should throw if no quantity is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const promise = sut.pay('any_value')
     expect(promise).rejects.toThrow(new MissingParamError('quantity'))
   })
 
   test('Should throw if no currency is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const promise = sut.pay('any_value', 'any_quantity')
     expect(promise).rejects.toThrow(new MissingParamError('currency'))
   })
 
   test('Should throw if no orderId is provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const promise = sut.pay('any_value', 'any_quantity', 'any_currency')
     expect(promise).rejects.toThrow(new MissingParamError('orderId'))
+  })
+
+  test('Should call StripeService with correct values', async () => {
+    const { sut, stripeServiceSpy } = makeSut()
+    await sut.pay('any_value', 'any_quantity', 'any_currency', 'any_orderId')
+    expect(stripeServiceSpy.value).toBe('any_value')
+    expect(stripeServiceSpy.quantity).toBe('any_quantity')
+    expect(stripeServiceSpy.currency).toBe('any_currency')
+    expect(stripeServiceSpy.orderId).toBe('any_orderId')
   })
 })
