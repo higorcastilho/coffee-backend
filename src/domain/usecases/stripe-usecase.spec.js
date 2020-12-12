4a8f7faf528b18c96a10c40c471022e1aff35552
Higor Castilho <castilhohf@gmail.com>
2020-12-12 00:16:35 -0300

    refactor: move StripeUseCase creation to a makeSut factory helper

--- src/domain/usecases/stripe-usecase.spec.js
+++ src/domain/usecases/stripe-usecase.spec.js
@@ -2,14 +2,30 @@ const MissingParamError = require('../../utils/errors/missing-param-error')
 
 class StripeUseCase {
   async pay (value, quantity, orderId) {
-    throw new MissingParamError('value')
+    if (!value) {
+      throw new MissingParamError('value')
+    }
+
+    if (!quantity) {
+      throw new MissingParamError('quantity')
+    }
   }
+}
+
+const makeSut = () => {
+  return new StripeUseCase()
 }
 
 describe('Stripe Usecase', () => {
   test('Should throw if no value is provided', async () => {
-    const sut = new StripeUseCase()
+    const sut = makeSut()
     const promise = sut.pay()
     expect(promise).rejects.toThrow(new MissingParamError('value'))
+  })
+
+  test('Should throw if no quantity is provided', async () => {
+    const sut = makeSut()
+    const promise = sut.pay('any_value')
+    expect(promise).rejects.toThrow(new MissingParamError('quantity'))
   })
 })
