2d6aa8eae8ae74b2e6a8c2cce6a8be9b4d79b9fa
Higor Castilho <castilhohf@gmail.com>
2020-12-13 06:28:03 -0300

    test: ensure StripeRouter returns 500 if no httpRequest is provided

--- src/presentation/routes/stripe-router.spec.js
+++ src/presentation/routes/stripe-router.spec.js
@@ -1,5 +1,6 @@
 const HttpResponse = require('../helpers/http-response')
 const { MissingParamError } = require('../../utils/errors')
+const { ServerError } = require('../errors')
 
 class StripeRouter {
   async route (httpRequest) {
@@ -88,5 +89,12 @@ describe('Stripe Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.error).toBe(new MissingParamError('orderId').message)
+  })
+
+  test('Should return 500 if no httpRequest is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body.error).toBe(new ServerError().message)
   })
 })
