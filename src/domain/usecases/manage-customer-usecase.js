52babc3fa9f9e1100e91c4c583c14ef83c3a1b32
Higor Castilho <castilhohf@gmail.com>
2020-12-21 08:19:09 -0300

    refactor: move sut (ManageCustomerUsecase) to its own file

---
+++ src/domain/usecases/manage-customer-usecase.js
@@ -0,0 +1,38 @@
+const { MissingParamError } = require('../../utils/errors')
+
+module.exports = class ManageCustomerUsecase {
+  constructor (loadUserByEmailRepository, createUserRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+    this.createUserRepository = createUserRepository
+  }
+
+  async returnOrCreateCustomer (name, email, phone, address, zip) {
+    if (!name) {
+      throw new MissingParamError('name')
+    }
+
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+
+    if (!phone) {
+      throw new MissingParamError('phone')
+    }
+
+    if (!address) {
+      throw new MissingParamError('address')
+    }
+
+    if (!zip) {
+      throw new MissingParamError('zip')
+    }
+
+    const customer = await this.loadUserByEmailRepository.load(email)
+
+    if (!customer) {
+      await this.createUserRepository.create(name, email, phone, address, zip)
+    }
+
+    return customer
+  }
+}
