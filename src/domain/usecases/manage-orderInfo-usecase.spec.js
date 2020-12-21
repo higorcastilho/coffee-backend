8d8b6e098a2e726950d224df125971adcd2e1086
Higor Castilho <castilhohf@gmail.com>
2020-12-21 20:41:20 -0300

    ensure ManageOrderInfoUseCase throws a customized MissingParamError if no param is provided

---
+++ src/domain/usecases/manage-orderInfo-usecase.spec.js
@@ -0,0 +1,71 @@
+const { MissingParamError } = require('../../utils/errors')
+
+class ManageOrderInfoUseCase {
+  constructor (createOrderRepository) {
+    this.createOrderRepository = createOrderRepository
+  }
+
+  async createOrder (paymentMethod, price, quantity, orderStatus, customerId) {
+    if (!paymentMethod) {
+      throw new MissingParamError('payment method')
+    }
+
+    if (!price) {
+      throw new MissingParamError('price')
+    }
+
+    if (!quantity) {
+      throw new MissingParamError('quantity')
+    }
+
+    if (!orderStatus) {
+      throw new MissingParamError('order status')
+    }
+
+    if (!customerId) {
+      throw new MissingParamError('customer id')
+    }
+
+    const order = this.createOrderRepository.create(paymentMethod, price, quantity, orderStatus, customerId)
+    return order.ops[0]._id
+  }
+}
+
+const makeSut = () => {
+  const sut = new ManageOrderInfoUseCase()
+  return {
+    sut
+  }
+}
+
+describe('Order Info Usecase', () => {
+  test('Shoul throw if no paymentMethod is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.createOrder()
+    expect(promise).rejects.toThrow(new MissingParamError('payment method'))
+  })
+
+  test('Shoul throw if no price is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.createOrder('any_payment_method')
+    expect(promise).rejects.toThrow(new MissingParamError('price'))
+  })
+
+  test('Shoul throw if no quantity is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.createOrder('any_payment_method', 'any_price')
+    expect(promise).rejects.toThrow(new MissingParamError('quantity'))
+  })
+
+  test('Shoul throw if no orderStatus is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.createOrder('any_payment_method', 'any_price', 'any_quantity')
+    expect(promise).rejects.toThrow(new MissingParamError('order status'))
+  })
+
+  test('Shoul throw if no customer id is provided', async () => {
+    const { sut } = makeSut()
+    const promise = sut.createOrder('any_payment_method', 'any_price', 'any_quantity', 'any_order_status')
+    expect(promise).rejects.toThrow(new MissingParamError('customer id'))
+  })
+})
