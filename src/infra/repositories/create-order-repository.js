092b609c4979f2dabf1b0e0e637ad336a30cc826
Higor Castilho <castilhohf@gmail.com>
2020-12-22 10:15:45 -0300

    refactor: move CreateOrderRepository to its own file

---
+++ src/infra/repositories/create-order-repository.js
@@ -0,0 +1,37 @@
+const MongoHelper = require('../helpers/mongo-helper')
+const { MissingParamError } = require('../../utils/errors')
+
+module.exports = class CreateOrderRepository {
+  async create (paymentMethod, price, quantity, orderStatus, customerId) {
+    if (!paymentMethod) {
+      throw new MissingParamError('payment method')
+    }
+
+    if (!price) {
+      throw new MissingParamError('price')
+    }
+
+    if (!quantity) {
+      throw new MissingParamError('quantity')
+    }
+
+    if (!orderStatus) {
+      throw new MissingParamError('order status')
+    }
+
+    if (!customerId) {
+      throw new MissingParamError('customer id')
+    }
+
+    const orderModel = await MongoHelper.getCollection('orders')
+    const order = await orderModel.insertOne({
+      paymentMethod,
+      price,
+      quantity,
+      orderStatus,
+      customerId
+    })
+
+    return order.ops[0]
+  }
+}
