4d9c4d8def1e1f0c4e8cd31fd343429dbd52124b
Higor Castilho <castilhohf@gmail.com>
2020-12-22 10:08:53 -0300

    test: ensure CreateOrderRepository returns a order if a order is successfully created

--- src/infra/repositories/create-order-repository.spec.js
+++ src/infra/repositories/create-order-repository.spec.js
@@ -1,6 +1,8 @@
 const MongoHelper = require('../helpers/mongo-helper')
 const { MissingParamError } = require('../../utils/errors')
 
+let orderModel
+
 class CreateOrderRepository {
   async create (paymentMethod, price, quantity, orderStatus, customerId) {
     if (!paymentMethod) {
@@ -23,8 +25,16 @@ class CreateOrderRepository {
       throw new MissingParamError('customer id')
     }
 
-    const order = await MongoHelper.getCollection('orders')
-    console.log(order)
+    const orderModel = await MongoHelper.getCollection('orders')
+    const order = await orderModel.insertOne({
+      paymentMethod,
+      price,
+      quantity,
+      orderStatus,
+      customerId
+    })
+
+    return order.ops[0]
   }
 }
 
@@ -37,33 +47,59 @@ const makeSut = () => {
 }
 
 describe('Create Order Repository', () => {
-  test('Shoul throw if no paymentMethod is provided', async () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    orderModel = await MongoHelper.getCollection('orders')
+  })
+
+  beforeEach(async () => {
+    await orderModel.deleteMany()
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should throw if no paymentMethod is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.create()
     expect(promise).rejects.toThrow(new MissingParamError('payment method'))
   })
 
-  test('Shoul throw if no price is provided', async () => {
+  test('Should throw if no price is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.create('any_payment_method')
     expect(promise).rejects.toThrow(new MissingParamError('price'))
   })
 
-  test('Shoul throw if no quantity is provided', async () => {
+  test('Should throw if no quantity is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.create('any_payment_method', 'any_price')
     expect(promise).rejects.toThrow(new MissingParamError('quantity'))
   })
 
-  test('Shoul throw if no orderStatus is provided', async () => {
+  test('Should throw if no orderStatus is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.create('any_payment_method', 'any_price', 'any_quantity')
     expect(promise).rejects.toThrow(new MissingParamError('order status'))
   })
 
-  test('Shoul throw if no customer id is provided', async () => {
+  test('Should throw if no customer id is provided', async () => {
     const { sut } = makeSut()
     const promise = sut.create('any_payment_method', 'any_price', 'any_quantity', 'any_order_status')
     expect(promise).rejects.toThrow(new MissingParamError('customer id'))
+  })
+
+  test('Should return a order if a order is successfully created', async () => {
+    const { sut } = makeSut()
+    const order = await sut.create('any_payment_method', 'any_price', 'any_quantity', 'any_order_status', 'any_customer_id')
+    expect(order).toEqual({
+      _id: order._id,
+      paymentMethod: 'any_payment_method',
+      price: 'any_price',
+      quantity: 'any_quantity',
+      orderStatus: 'any_order_status',
+      customerId: 'any_customer_id'
+    })
   })
 })
