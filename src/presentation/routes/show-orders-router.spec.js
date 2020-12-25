298e854627cc5e54b256f61a88d057826f9cd8f3
Higor Castilho <castilhohf@gmail.com>
2020-12-25 10:56:01 -0300

    test: ensure ShowOrdersRouter returns 500 if no httpRequest is provided or httpRequest has no body

--- src/presentation/routes/show-orders-router.spec.js
+++ src/presentation/routes/show-orders-router.spec.js
@@ -1,16 +1,21 @@
 const HttpResponse = require('../helpers/http-response')
+const { ServerError } = require('../errors')
 const { MissingParamError } = require('../../utils/errors')
 
 describe('Show Orders Router', () => {
   class ShowOrdersRouter {
     async route (httpRequest) {
-      const { limit, offset } = httpRequest.query
-      if (!limit) {
-        return HttpResponse.badRequest(new MissingParamError('limit'))
-      }
+      try {
+        const { limit, offset } = httpRequest.query
+        if (!limit) {
+          return HttpResponse.badRequest(new MissingParamError('limit'))
+        }
 
-      if (!offset) {
-        return HttpResponse.badRequest(new MissingParamError('offset'))
+        if (!offset) {
+          return HttpResponse.badRequest(new MissingParamError('offset'))
+        }
+      } catch (error) {
+        return HttpResponse.serverError()
       }
     }
   }
@@ -44,5 +49,20 @@ describe('Show Orders Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.error).toBe(new MissingParamError('offset').message)
+  })
+
+  test('Should return 500 if no httpRequest is provided', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.route()
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body.error).toBe(new ServerError().message)
+  })
+
+  test('Should return 500 if httpRequest has no body', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {}
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body.error).toBe(new ServerError().message)
   })
 })
