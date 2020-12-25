5d281f28479c16c84264d7e46478366b4e1aa0e9
Higor Castilho <castilhohf@gmail.com>
2020-12-25 10:36:31 -0300

    test: ensure ShowOrdersRouter returns 400 if no offset is provided

--- src/presentation/routes/show-orders-router.spec.js
+++ src/presentation/routes/show-orders-router.spec.js
@@ -4,9 +4,14 @@ const { MissingParamError } = require('../../utils/errors')
 describe('Show Orders Router', () => {
   class ShowOrdersRouter {
     async route (httpRequest) {
-      // const { limit, offset } = httpRequest.query
+      const { limit, offset } = httpRequest.query
+      if (!limit) {
+        return HttpResponse.badRequest(new MissingParamError('limit'))
+      }
 
-      return HttpResponse.badRequest(new MissingParamError('limit'))
+      if (!offset) {
+        return HttpResponse.badRequest(new MissingParamError('offset'))
+      }
     }
   }
 
@@ -27,5 +32,17 @@ describe('Show Orders Router', () => {
     const httpResponse = await sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body.error).toBe(new MissingParamError('limit').message)
+  })
+
+  test('Should return 400 if no offset is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      query: {
+        limit: 'any_limit'
+      }
+    }
+    const httpResponse = await sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body.error).toBe(new MissingParamError('offset').message)
   })
 })
