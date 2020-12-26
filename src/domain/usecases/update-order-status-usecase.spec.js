cce6e5774c3821d0638db603b5ce1f7056073172
Higor Castilho <castilhohf@gmail.com>
2020-12-26 16:39:34 -0300

    test: ensure UpdateOrderStatusUseCase returns an empty object if the order is updated successfully

--- src/domain/usecases/update-order-status-usecase.spec.js
+++ src/domain/usecases/update-order-status-usecase.spec.js
@@ -1,6 +1,11 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class UpdateOrderStatusUseCase {
+  constructor (updateOrderStatusRepository, orderStatus = 0) {
+    this.updateOrderStatusRepository = updateOrderStatusRepository
+    this.orderStatus = orderStatus
+  }
+
   async update (success, canceled, orderId) {
     if (!success) {
       throw new MissingParamError('success')
@@ -13,12 +18,48 @@ class UpdateOrderStatusUseCase {
     if (!orderId) {
       throw new MissingParamError('orderId')
     }
+
+    if (success && !canceled) {
+      this.orderStatus = 1
+    }
+
+    if (!success && canceled) {
+      this.orderStatus = 0
+    }
+
+    const response = await this.updateOrderStatusRepository.update(this.orderStatus, orderId)
+    return response
   }
 }
 
+const makeUpdateOrderStatusRepository = () => {
+  class UpdateOrderStatusRepositorySpy {
+    async update (orderStatus, orderId) {
+      this.orderStatus = orderStatus
+      this.orderId = orderId
+      return this.response
+    }
+  }
+
+  return new UpdateOrderStatusRepositorySpy()
+}
+
+const makeUpdateOrderStatusRepositoryWithError = () => {
+  class UpdateOrderStatusRepositorySpy {
+    async update () {
+      throw new Error()
+    }
+  }
+
+  return new UpdateOrderStatusRepositorySpy()
+}
+
 const makeSut = () => {
-  const sut = new UpdateOrderStatusUseCase()
+  const updateOrderStatusRepositorySpy = makeUpdateOrderStatusRepository()
+  updateOrderStatusRepositorySpy.response = {}
+  const sut = new UpdateOrderStatusUseCase(updateOrderStatusRepositorySpy)
   return {
+    updateOrderStatusRepositorySpy,
     sut
   }
 }
@@ -40,5 +81,30 @@ describe('Update Order Status UseCase', () => {
     const { sut } = makeSut()
     const promise = sut.update('any_boolean', '!any_boolean')
     expect(promise).rejects.toThrow(new MissingParamError('orderId'))
+  })
+
+  test('Should call UpdateOrderStatusRepository with correct values', async () => {
+    const { sut, updateOrderStatusRepositorySpy } = makeSut()
+    await sut.update('any_boolean', '!any_boolean', 'any_order_id')
+    expect(updateOrderStatusRepositorySpy.orderStatus).toBe(sut.orderStatus)
+    expect(updateOrderStatusRepositorySpy.orderId).toBe('any_order_id')
+  })
+
+  test('Should return an empty object if correct values are provided and is updated successfully', async () => {
+    const { sut } = makeSut()
+    const response = await sut.update('any_boolean', '!any_boolean', 'any_order_id')
+    expect(response).toEqual({})
+  })
+
+  test('Should throw if any dependency throws', async () => {
+    const updateOrderStatusRepositorySpyWithError = makeUpdateOrderStatusRepositoryWithError()
+    const suts = [].concat(
+      new UpdateOrderStatusUseCase(updateOrderStatusRepositorySpyWithError)
+    )
+
+    for (const sut of suts) {
+      const promise = sut.update('any_boolean', '!any_boolean', 'any_order_id')
+      expect(promise).rejects.toThrow()
+    }
   })
 })
